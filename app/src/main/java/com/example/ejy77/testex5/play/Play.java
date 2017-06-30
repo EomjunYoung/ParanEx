3a6030b31addab94417a1edf8b768b21a66ea86d
dreamline91 <dreamline91@naver.com>
2017-06-30 21:56:50 +0900

    complete making Play class
    it is like a Start UP class which we learned from Domain Analysis Software Design class.

--- app/src/main/java/com/example/ejy77/testex5/play/Play.java
+++ app/src/main/java/com/example/ejy77/testex5/play/Play.java
@@ -1,8 +1,23 @@
 package com.example.ejy77.testex5.play;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.ejy77.testex5.MainActivity;
+
 /**
  * Created by dream on 2017-06-30.
  */
 
-public class Play {
+public class Play extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        finish();
+    }
 }
