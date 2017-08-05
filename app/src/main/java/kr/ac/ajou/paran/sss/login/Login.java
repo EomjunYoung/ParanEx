8fb1a5a64a0b8c7870692dd772462410b4f5facf
dreamline91 <dreamline91@naver.com>
2017-08-05 19:27:37 +0900

    simple Profile

--- app/src/main/java/kr/ac/ajou/paran/sss/login/Login.java
+++ app/src/main/java/kr/ac/ajou/paran/sss/login/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +20,8 @@ import java.net.URLEncoder;
 
 import kr.ac.ajou.paran.R;
 import kr.ac.ajou.paran.sss.main.Main;
+import kr.ac.ajou.paran.util.HTTP;
+import kr.ac.ajou.paran.util.User;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,8 +29,6 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     private EditText editID, editPWD;
 
     private String cookie;
-
-    private final static String MOBILE = "https://mb.ajou.ac.kr";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,80 +50,13 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
-    public HttpURLConnection makeConnection(URL url) {
-        HttpURLConnection con;
-        try {
-            con = (HttpURLConnection) url.openConnection();
-            con.setRequestProperty("Accept-Language", "ko-kr,ko;q=0.8,en-us;q=0.5,en;q=0.3");
-            con.setRequestProperty("Accept-Upgrade-Insecure-Requests", "1");
-            return con;
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public String loginMobilePara(String id, String pwd) {
-        String parameter;
-        try {
-            parameter = URLEncoder.encode("id", "UTF-8") + "=" + URLEncoder.encode(id, "UTF-8");
-            parameter += "&" + URLEncoder.encode("passwd", "UTF-8") + "=" + URLEncoder.encode(pwd, "UTF-8");
-            parameter += "&" + URLEncoder.encode("rememberMe", "UTF-8") + "=" + URLEncoder.encode("N", "UTF-8");
-            parameter += "&" + URLEncoder.encode("platformType", "UTF-8") + "=" + URLEncoder.encode("A", "UTF-8");
-            parameter += "&" + URLEncoder.encode("deviceToken", "UTF-8") + "="
-                    + URLEncoder.encode(
-                    "APA91bHLPmSyNjpcYoWFGEdHIJiNYIConV_kcx6DdXpuzH7o3OimWFLDZ_J60nK9EQRdmyMFHJbJsoLC2y-tKceiYJTmAl_0L7ZtkkZF8fgKR0VMwMkJy4o",
-                    "UTF-8");
-            return parameter;
-        } catch (UnsupportedEncodingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public String postLoginMOBILE(URL url, String id, String pwd) {
-        try {
-            HttpURLConnection con = makeConnection(url);
-            con.setDoOutput(true);
-            con.setRequestMethod("POST");
-            OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
-            wr.write(loginMobilePara(id, pwd));
-            wr.flush();
-            String cookie = con.getHeaderFields().get("Set-Cookie").get(2);
-            cookie = cookie.substring(0, cookie.indexOf(";"));
-            con.disconnect();
-            return cookie;
-        } catch (IOException | IndexOutOfBoundsException e) {
-            // TODO Auto-generated catch block
-            return null;
-        }
-    }
-
-    public String loginMobile(String id, String pwd, String cookie) {
-        try {
-            cookie = postLoginMOBILE(new URL(MOBILE + "/mobile/login.json"), id, pwd);
-
-            return cookie;
-        } catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return null;
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return null;
-        }
-    }
-
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.loginButton:
-                if ((cookie = loginMobile(editID.getText().toString(), editPWD.getText().toString(), cookie)) != null) {
+                if ((cookie = HTTP.loginAjou(editID.getText().toString(), editPWD.getText().toString())) != null) {
                     Toast.makeText(getApplicationContext(), "Success in login", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(this, Main.class));
+                    startActivity(new Intent(this, Main.class).putExtra("Cookie",cookie));
                     overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                     finish();
                 }else
--- app/src/main/java/kr/ac/ajou/paran/sss/main/Main.java
+++ app/src/main/java/kr/ac/ajou/paran/sss/main/Main.java
@@ -1,8 +1,16 @@
 package kr.ac.ajou.paran.sss.main;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import kr.ac.ajou.paran.R;
+import kr.ac.ajou.paran.util.HTTP;
+import kr.ac.ajou.paran.util.User;
 
 
 /**
@@ -10,10 +18,27 @@ import kr.ac.ajou.paran.R;
  */
 
 public class Main extends AppCompatActivity {
+
+    private User user;
+
+    private String cookie;
+
+    private ImageView imagePicture;
+    private TextView textUser;
+
     @Override
     protected void onCreate(Bundle savedlnstanceState) {
         super.onCreate(savedlnstanceState);
         setContentView(R.layout.activity_main);
 
+        cookie = getIntent().getStringExtra("Cookie");
+
+        imagePicture = (ImageView)findViewById(R.id.imagePicture);
+        textUser = (TextView)findViewById(R.id.textUser);
+        User user = HTTP.printUser(cookie);
+        HTTP.logOut(cookie);
+        if(user.getNumber()!=0)
+            imagePicture.setImageBitmap(HTTP.printPicture(user.getNumber(), (int) (((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay().getHeight() * 0.3)));
+        textUser.setText(user.getName()+"\n"+user.getNumber()+"\n"+user.getGrade()+"학년"+"\n"+user.getMajor());
     }
 }
