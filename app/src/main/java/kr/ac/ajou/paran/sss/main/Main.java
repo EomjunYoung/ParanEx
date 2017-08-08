9e1917d18921b65660201acc2f795c9c7f616eca
EomjunYoung <theoneday737@naver.com>
2017-08-08 21:38:29 +0900

    add db
    dbdb

--- app/src/main/java/kr/ac/ajou/paran/sss/main/Main.java
+++ app/src/main/java/kr/ac/ajou/paran/sss/main/Main.java
@@ -1,14 +1,21 @@
 package kr.ac.ajou.paran.sss.main;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
 
 import kr.ac.ajou.paran.R;
 import kr.ac.ajou.paran.util.DialogType;
@@ -32,10 +39,23 @@ public class Main extends AppCompatActivity {
     private ImageView imageLogo;
     private TextView textUser;
 
+    final String string = "mydb.db";
+    SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedlnstanceState) {
         super.onCreate(savedlnstanceState);
         setContentView(R.layout.activity_main);
+
+        db = openOrCreateDatabase(string, MODE_PRIVATE, null);
+        StringBuilder sb = new StringBuilder();
+        sb.append("CREATE TABLE IF NOT EXISTS dbtable2(");
+        sb.append("_id INTEGER PRIMARY KEY autoincrement, ");
+        sb.append("name TEXT, ");
+        sb.append("studentId TEXT, ");
+        sb.append("grade TEXT, ");
+        sb.append("major TEXT)");
+
 
         /* 쿠키를 가져옴 */
         cookie = getIntent().getStringExtra("Cookie");
@@ -79,5 +99,38 @@ public class Main extends AppCompatActivity {
         imageLogo.setY(height*0.2f);
         /*로고 출력*/
 
+
+        try
+        {
+            db.execSQL(sb.toString());
+            String sql = "insert into dbtable2(name, studentId, grade, major) values('"+user.getName()+"', '"+user.getNumber()+"', '"+user.getGrade()+"', '"+user.getMajor()+"')";
+            db.execSQL(sql);
+
+        }
+
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public String printData() //db저장된거 확인
+    {
+        String str = "select *from dbtable2 order by _id desc";
+        Cursor cursor = db.rawQuery(str, null);
+
+        String str3 = "";
+        while(cursor.moveToNext())
+        {
+
+
+            str3 += "id:" + cursor.getInt(0) + "이름:" + cursor.getString(1) + "학번:"
+                    + cursor.getString(2) + "학년:" + cursor.getString(3) + "전공:" + cursor.getString(4) +"\n";
+
+        }
+
+        return str3;
     }
 }
