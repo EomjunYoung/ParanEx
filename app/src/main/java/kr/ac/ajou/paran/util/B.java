d199ca8fc178a38b9a09a4894a693868aa6c4d93
dreamline91 <dreamline91@naver.com>
2017-08-24 15:47:31 +0900

    edit code

--- app/src/main/java/kr/ac/ajou/paran/util/B.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.ajou.paran.util;
-
-/**
- * Created by dream on 2017-08-24.
- */
-
-public class B {
-    int test;
-}
--- app/src/main/java/kr/ac/ajou/paran/util/c.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.ajou.paran.util;
-
-/**
- * Created by user on 2017-08-24.
- */
-
-public class c {
-}
