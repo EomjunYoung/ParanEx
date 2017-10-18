b9d26e04bbb176de932abf88729c323a188b0286
jaejunha <dreamline91@naver.com>
2017-10-18 16:35:07 +0900

    apply openCV program at window

--- python/server/views.py
+++ python/server/views.py
@@ -3,11 +3,23 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+import base64
+import os
 
 @csrf_exempt 
 def postTable(request):
-    if request.method == 'POST':
-        print(request.POST.get('data',''))
+    data = request.POST.get('data','')
+    data = data.replace(' ','+')
+    if data :
+        decoded_data = base64.b64decode(data)
+        output = open('../c++/OpenCV/x64/Debug/test.jpg','wb')
+        output.write(decoded_data)
+        print 'ok data is saved'
+        result = os.popen('start /b ../c++/OpenCV/x64/Debug/OpenCV.exe').read()
+        print result
+    # if no data comming
+    else :
+        print 'no data'
     return render(request,'server/template/index.html')
 
 # Create your views here.
