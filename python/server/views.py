1689ccb95a6d0573459361559ac048e3a937ef18
jaejunha <dreamline91@naver.com>
2017-10-20 22:34:02 +0900

    usbject OCR at python

--- python/server/views.py
+++ python/server/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 import base64
 import os
+import httplib
+import json
 
 @csrf_exempt 
 def postTable(request):
@@ -12,14 +14,40 @@ def postTable(request):
     data = data.replace(' ','+')
     if data :
         decoded_data = base64.b64decode(data)
-        output = open('../c++/OpenCV/x64/Debug/test.jpg','wb')
+        output = open('test.jpg','wb')
         output.write(decoded_data)
         print 'ok data is saved'
         result = os.popen('start /b ../c++/OpenCV/x64/Debug/OpenCV.exe').read()
         print result
+	
+	file = open('key.txt','rb')
+	key = file.read()
+	file.close()
+	for i in range(1,5):
+		image = open(str(i)+'.jpg','rb')
+       		encoded_image = base64.b64encode(image.read())
+		c = httplib.HTTPSConnection("vision.googleapis.com")
+
+		params = '{\"requests\":[{\"image\":{\"content\":\"'
+		params += encoded_image
+		params += '\"},\"features\" : {\"type\":\"TEXT_DETECTION\",\"maxResults\" : 10}}]}'
+
+		c.request("POST", "/v1/images:annotate?key=" + key, params)
+		response = c.getresponse()
+		print response.status, response.reason
+		data = response.read()
+		j = json.loads(data)
+		try:
+			for t in j['responses'][0]['textAnnotations']:
+				print t['description']
+		except:
+			print ''
+		c.close()
+		image.close()
     # if no data comming
     else :
         print 'no data'
+
     return render(request,'server/template/index.html')
 
 # Create your views here.
