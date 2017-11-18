662c1771408d372ad8a3b8e0160a87e2ed056def
jaejunha <dreamline91@naver.com>
2017-11-19 00:14:25 +0900

    modify package

--- android/app/src/main/java/kr/ac/ajou/paran/stage/main/dialog/CheckSavedTable.java
+++ android/app/src/main/java/kr/ac/ajou/paran/stage/main/dialog/CheckSavedTable.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
-import kr.ac.ajou.paran.stage.main.function.TimeTable;
+import kr.ac.ajou.paran.stage.main.function.timeTable.TimeTable;
 import kr.ac.ajou.paran.util.Recognizer;
 
 /**
